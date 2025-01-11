0300fa2f614847171b96aaea71fa55d560452392
Jalil Abdullayev <abdullacelil00@gmail.com>
2025-01-11 11:53:48 +0400

    Implement tabs navigation

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,18 +10,6 @@ const Index = () =>
         <Text className='font-bold text-3xl my-10 font-rubik'>
             Welcome to ReState
         </Text>
-        <Link href='/sign-in'>
-            Sign In
-        </Link>
-        <Link href='/explore'>
-            Explore
-        </Link>
-        <Link href='/profile'>
-            Profile
-        </Link>
-        <Link href='/properties/1'>
-            Property
-        </Link>
     </View>;
 
 export default Index;
